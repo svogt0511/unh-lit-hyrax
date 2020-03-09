210f9d9103a106e5b48e5cb01602ee58013d222d
root <root@digitalcollections-test.unh.edu>
2020-03-09 16:25:59 -0400

    Issue #1: fixed login problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
 
   helper Zizia::Engine.helpers
 
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery unless: -> { request.format.json? }
+  skip_before_action :verify_authenticity_token
 end
