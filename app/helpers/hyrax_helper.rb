b784bdf0d6dc0a5ee121d3859e10d3430a72cec3
Suzanne Vogt <suzanne.vogt@unh.edu>
2020-04-06 17:37:44 -0400

    Issue #25 - Branding Hyrax - Set page titles properly.

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,9 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  # Change/override the join string. (from " // " to ' | ')
+  def construct_page_title(*elements)
+    (elements.flatten.compact).join(' | ')
+  end
 end
