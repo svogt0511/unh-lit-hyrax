38ab445978c2dc4fd9bffb5f442f250d626ae066
Suzanne Vogt <suzanne.vogt@unh.edu>
2020-07-17 17:45:40 -0400

    Issue #25 - Customize Zizia importer. (Parameterize file_attachments_path - IMPORT_PATH env variable.  It was hard-coded in several places in the zizia importer. Now it can be set by simply setting the env var IMPORT_PATH or leaving it as the /opt/data default.)

--- app/overrides/zizia/hyrax_record_importer.rb
+++ app/overrides/zizia/hyrax_record_importer.rb
@@ -7,14 +7,14 @@ class Zizia::HyraxRecordImporter
       ENV['IMPORT_PATH'] || '/opt/data'
     end
 
-        ##
+    ##
     # Within the directory specified by ENV['IMPORT_PATH'], find the first
     # instance of a file matching the given filename.
     # If there is no matching file, raise an exception.
     # @param [String] filename
     # @return [String] a full pathname to the found file
     def find_file_path(filename)
-      filepath = Dir.glob("#{ENV['IMPORT_PATH']}/**/#{filename}").first
+      filepath = Dir.glob(file_attachments_path + "/**/#{filename}").first
 
       raise "Cannot find file #{filename}... Are you sure it has been uploaded and that the filename matches?" if filepath.nil?
       filepath
